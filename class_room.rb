91cac01c537aea5688bc7ab77e2b9da618b7d3b5
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-08-25 15:27:15 +0300

    Add has-many/belongs-to relationship between Classroom and Student

--- class_room.rb
+++ class_room.rb
@@ -1,7 +1,13 @@
 class ClassRoom
   def initialize(label)
     @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    students.push(student)
   end
 
   attr_accessor :label
+  attr_reader :students
 end
