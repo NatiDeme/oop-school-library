41ce7e47e932fa63aa58e13c6956780ec938581b
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-08-25 13:12:40 +0300

    Add final testing file to check the output

--- TrimmerDecorator.rb
+++ TrimmerDecorator.rb
@@ -3,6 +3,6 @@ require './base_decorator'
 class TrimmerDecorator < BaseDecorator
   def correct_name
     name = @nameable.correct_name
-    name[0..10]
+    name[0..9]
   end
 end
--- testing.rb
+++ testing.rb
@@ -1,6 +1,10 @@
-text = 'natnael demelash'
-puts text.size
-puts text[0..11].gsub(/\s\w+\s*$/, '')
-me = text[0..11].gsub(/\s\w+\s*$/, '')
-puts text[0..11]
-puts me.size
+require './person'
+require './CapitalizeDecorator'
+require './TrimmerDecorator'
+
+person = Person.new(22, 'maximilianussss')
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts capitalizedTrimmedPerson.correct_name
